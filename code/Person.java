d87c4b64bcff4ba5cc4a41963e8d98053bdb09ba
flaviacoelho <38075822+flaviacoelho@users.noreply.github.com>
2018-09-09 11:24:37 -0300

    Add files via upload

--- code/Person.java
+++ code/Person.java
@@ -1,25 +1,32 @@
 package main.java;
 
 public class Person {
-				
+		
 	private String name;
-	private TelephoneNumber officeTelephone = new TelephoneNumber();
+	private String officeAreaCode;
+	private String officeNumber;
 	
 	public String getName() {
 		return name;
 	}
 	
-	public String getTelephoneNumber(){
-		return officeTelephone.getTelephoneNumber();
+	public String getTelephoneNumber() {
+		return ("(" + officeAreaCode + ") " + officeNumber);
 	}
 	
-	TelephoneNumber getOfficeTelephone() {
-		return officeTelephone;
+	String getOfficeAreaCode() {
+		return officeAreaCode;
+	}
+	
+	void setOfficeAreaCode(String arg) {
+		officeAreaCode = arg;
+	}
+	
+	String getOfficeNumber() {
+		return officeNumber;
+	}
+	
+	void setOfficeNumber(String arg) {
+		officeNumber = arg;
 	}	
-	
-	@Override
-	public String toString() {
-		return "Name: " + getName() + " Office Telephone: " + getTelephoneNumber();
-	}
-	
 }
