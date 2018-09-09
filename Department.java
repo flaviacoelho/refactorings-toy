6bb92583c93e8429e007dafb83c9b036613fd80a
flaviacoelho <38075822+flaviacoelho@users.noreply.github.com>
2018-09-09 12:31:24 -0300

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class Department extends Party{
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ Party.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class Party {
+	private String totalAnnualCost;
+	
+	public String getTotalAnnualCost() {
+		return totalAnnualCost;
+	}
+
+	public void setTotalAnnualCost(String totalAnnualCost) {
+		this.totalAnnualCost = totalAnnualCost;
+	}
+
+}
