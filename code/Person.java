7f8fdeec2fa30c2fa60e0e810ae2f78c5826a120
flaviacoelho <38075822+flaviacoelho@users.noreply.github.com>
2018-09-14 14:44:19 -0300

    Add files via upload

--- code/Person.java
+++ code/Person.java
@@ -19,5 +19,14 @@ public class Person {
 }
 
 class OnePerson{
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	
 }
