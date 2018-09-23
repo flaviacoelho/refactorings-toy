38b85b5611adda1181628596aac6a0eeda96f994
flaviacoelho <38075822+flaviacoelho@users.noreply.github.com>
2018-09-23 19:00:39 -0300

    Add files via upload

---
+++ code/TelephoneNumber.java
@@ -0,0 +1,34 @@
+package main.java.util;
+
+public class TelephoneNumber {
+	
+	private String number;
+	private String areaCode;
+	
+	public String getTelephoneNumber() {
+		return ("(" + areaCode + ") " + number);
+	}
+	
+	String getAreaCode() {
+		return areaCode;
+	}
+	
+	void setAreaCode(String arg) {
+		areaCode = arg;
+	}
+	
+	String getNumber() {
+		return number;
+	}
+	
+	void setNumber(String arg) {
+		number = arg;
+	}
+	
+	public void testExtract(Person p) {
+		setAreaCode("00");
+		System.out.println(p.getName());
+		setAreaCode("88");
+	}
+		
+}
