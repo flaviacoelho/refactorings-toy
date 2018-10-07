940bae12ca19b8df2e6125ab54035d36e2374107
flaviacoelho <flavinha.coelho@gmail.com>
2018-10-07 20:43:29 -0300

    v 0.2

--- code/A.java
+++ code/A.java
@@ -2,9 +2,6 @@ package main.java.code;
 
 public class A {
 	public int getZeroValue() {
-		return isZero() ? 1 : 0;
-	}
-	public boolean isZero() {
-		return (0 == 0);
+		return (0 == 0) ? 1 : 0;
 	}
 }
