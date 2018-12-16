d8df02d791965144119e46f3ffaa8641f39f2dba
flaviacoelho <flavinha.coelho@gmail.com>
2018-12-16 16:18:46 -0200

    v 5.3

--- code/package4/R.java
+++ code/package4/R.java
@@ -1,6 +1,5 @@
 package main.java.code.package4;
 
 public class R {
-	//Move and Rename Attribute...
-	String s;
+	
 }
--- code/package4/S.java
+++ code/package4/S.java
@@ -1,5 +1,6 @@
 package main.java.code.package4;
 
 public class S {
-		
+	//Move and Rename Attribute...
+	String s;	
 }
